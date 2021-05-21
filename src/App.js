c62050e3ad6478cc5a19f08ce9653a4feecf9e99
Your Name <you@example.com>
2021-05-21 11:00:14 +0100

    Logout Button Setup to Remove Token

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import PrivateRoute from './components/PrivateRoute';
+import { axiosWithAuth } from './helpers/axiosWithAuth';
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
 function App() {
+
+  const logout = () => {
+    axiosWithAuth().post('/logout')
+      .then(res=> {
+        localStorage.removeItem("token");
+        window.location.href = "/";
+      })
+      .catch(err=> {
+        console.log(err);
+      })
+    };
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <Link data-testid="logoutButton" href={logout}>logout</Link>
         </header> 
-
-        <Route exact path="/" component={Login} />
+        <Switch>
+          <PrivateRoute path="/bubblePage" component={BubblePage} />
+          <Route exact path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
@@ -24,3 +42,4 @@ export default App;
 //Task List:
 //1. Render BubblePage as a PrivateRoute
 //2. Build the logout button to remove the localStorage Item.
+
