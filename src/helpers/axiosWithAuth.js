8e83829d5b020451a15136331bee486b3395ff54
Your Name <you@example.com>
2021-05-21 10:39:06 +0100

    axiosWithAuth Setup

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,3 +2,14 @@ import axios from "axios";
 
 //Task List:
 //Build and export a function used to send in our authorization token
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        baseURL: 'http://localhost:5000/api'
+    });
+}
